a4ca87e7a67e038b3aea39a26093a266ee747c6d
Sparsh <sparshaggarwal561@gmail.com>
2025-02-18 13:33:32 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "GenAI Project",
+    version = '0.0.0',
+    author = 'Sparsh Aggarwal',
+    autor_email = 'sparshaggarwal561@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
